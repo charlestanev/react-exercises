be8987b7b1fde543fa038cc400831f3e9fc57e7d
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2025-03-13 12:27:13 +0200

    Add fadeIn animation and keyframes to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
 };
